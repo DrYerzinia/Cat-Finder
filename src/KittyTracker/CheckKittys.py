e0361e502dccae66ef31ad01a3a3ff1066a62583
Michael Marques <dryerzinia@gmail.com>
2015-02-05 11:16:12 -0700

    Added code for monitoring kittys with BLE enabled linux computer (i.e. Raspberry PI with BLE dongle).  Can text you if kitty is not detected for predefined amount of time.

---
+++ src/KittyTracker/CheckKittys.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+from threading import Timer, Thread
+import time
+import SendMail
+
+class CheckKittys(Thread):
+	def run(self):
+		while self.running:
+			now = datetime.now()
+			for k in self.kittys:
+				diff = now - k.lastHeard
+				diff = diff.days * 86400 + diff.seconds
+				print 'Seconds since ' , k.name , ' heard: ', diff
+				if diff > k.ttw:
+					message = 'Alert: ' + k.name + ' NOT DETECTED for ' + str(diff) + ' SECONDS!'
+					SendMail.sendMail(message)
+					k.incrementTTW();
+			time.sleep(20)
+
